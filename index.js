b7a1eecb9da62e78b9b24500d66ae584d02310d7
Karrar Hussein <karraroo567@gmail.com>
2022-04-01 13:17:32 +0200

    add active states to the buttons

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const ratingForm = document.getElementById("rating-form");
 const ratingBtns = document.getElementById("rating-container").children;
 const ratingBtnsList = Array.from(ratingBtns);
 let selected = 0;
 let rating = 0;
+
+console.log(ratingBtnsList[0].value);
 
 function removeSelected(array, removedClass) {
   array.forEach((element) => {
@@ -16,9 +19,16 @@ ratingBtnsList.forEach((btn) => {
     if (selected === 0) {
       btn.classList.add("selected");
       selected = 1;
+      rating = btn.value;
     } else if (selected === 1) {
       removeSelected(ratingBtnsList, "selected");
       btn.classList.add("selected");
+      rating = btn.value;
     }
   });
+});
+
+ratingForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(rating);
 });
